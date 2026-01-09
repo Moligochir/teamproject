8ba4b705719a2ea7d7ee2371a4ca2c798ccffa33
25LP8334 25LP8334 <25LP8334@G301-CC0196.local>
2026-01-09 12:12:03 +0800

    adoptyn create

--- backend/src/resolvers/adopt/create-adopt.ts
+++ backend/src/resolvers/adopt/create-adopt.ts
@@ -1,30 +1,19 @@
 import { Request, Response } from "express";
 import { AdoptSchemaModel } from "../../models/model-apopt";
 
-type CreateAdopt = {
-  name: string;
-  age: number;
-  gender: string;
-  image: string;
-  breed: string;
-  description: string;
-  adoptType: string;
-};
-export const CreateAdopt = async (
-  req: Request<{}, {}, CreateAdopt>,
-  res: Response
-) => {
-  const { name, age, gender, image, breed, description, adoptType } = req.body;
+export const CreateAdopt = async (req: Request, res: Response) => {
+  const newAdopt = req.body;
   try {
     const dbAdopt = await AdoptSchemaModel.create({
-      name,
-      age,
-      gender,
-      image,
-      breed,
-      description,
-      adoptType,
+      petType: newAdopt.petType,
+      userId: newAdopt.userId,
+      name: newAdopt.name,
+      age: newAdopt.age,
+      image: newAdopt.image,
+      breed: newAdopt.breed,
+      description: newAdopt.description,
     });
+    res.status(200).json("success");
   } catch (e: unknown) {
     res.status(500).json({ message: (e as Error).message });
     console.log(e);
