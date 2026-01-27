47dea6693db7eddfb369a74c7443387ff9f74d28
25LP9111 25LP9111 <25LP9111@admins-iMac.local>
2026-01-27 10:30:13 +0800

    back2

--- backend/src/resolvers/LostFound/lol.ts
+++ backend/src/resolvers/LostFound/lol.ts
@@ -3,6 +3,4 @@ import { LostFoundModel } from "../../models/LostFoundModel";
 
 export const getLostFound = async (req: Request, res: Response) => {
   const dbLostFound = await LostFoundModel.find();
-
-  res.status(200).json(dbLostFound);
 };
