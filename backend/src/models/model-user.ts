fb379077d016f2a4f6af66f5a50c6ee1042cf1c7
25LP8334 25LP8334 <25LP8334@G301-CC0196.local>
2026-02-04 21:01:24 +0800

    detail

--- backend/src/models/model-user.ts
+++ backend/src/models/model-user.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
-  id: ObjectId,
   clerkId: {
     type: String,
     required: true,
